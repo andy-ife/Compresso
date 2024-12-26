b61c1b1847f5508c04b21947006ee48673329182
Ifeoluwa Andrew Omole <iomole3@gmail.com>
2024-12-26 23:11:26 +0100

    setup theme

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,29 @@ class MyApp extends StatelessWidget {
       title: 'Compresso',
       theme: ThemeData(
         useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo)
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo[600]!),
+        textTheme: const TextTheme(
+          titleSmall: TextStyle(
+            fontFamily: 'Montserrat',
+            fontSize: 16.0,
+            fontWeight: FontWeight.w600
+          ),
+          
+          displaySmall: TextStyle(
+            fontFamily: 'Open Sans'
+          ),
+
+          bodyMedium: TextStyle(
+            fontFamily: 'Open Sans',
+          ),
+
+          bodySmall: TextStyle(
+            fontFamily: 'Open Sans',
+            fontSize: 12.0,
+          )
+        )
       ),
+      
       home: const HomePage(),
     );
   }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -5,6 +5,8 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder(color: Colors.red);
+    return const Scaffold(
+      
+    );
   }
 }
